f0ea434135cd85dee6858b7af9fb60f097aa9584
Aman Jain <amanjain051106@gmail.com>
2026-09-25 14:48:04 +0530

    Time: 2 ms (97.59%), Space: 72.6 MB (30.99%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+         int five =0;
+         int ten =0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                five++;
+            }
+           else if(bills[i]==10){
+                if(five ==0){
+                    return false;
+                }
+                ten++;
+                five--;
+            }
+            else if(bills[i]==20){
+             if(five >0 && ten >0){
+                ten--;
+                five--;
+             }
+            else if(five>=3){
+                five--;
+                five--;
+                five--;
+             }
+             else{
+            return false;
+            }
+        }
+        }
+        return true;
+    }
+}
